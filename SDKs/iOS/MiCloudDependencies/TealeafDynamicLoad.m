abe8a50ed5ad3657a2fc4a3cbdec07746a07f692
Shridhar Damale <sdamale@us.ibm.com>
2017-11-20 10:54:33 +0530

    workitem 279514 : Xcode Compilation error for dynamic projects - remove module name. remode auto loading from dynamic load workitem 279514

--- SDKs/iOS/MiCloudDependencies/TealeafDynamicLoad.m
+++ SDKs/iOS/MiCloudDependencies/TealeafDynamicLoad.m
@@ -1,14 +1,14 @@
 #import "TealeafDynamicLoad.h"
-#import <Tealeaf.framework/Headers/TLFApplicationHelper.h>
 @implementation TealeafDynamicLoad
 + (void)load {
 #ifdef DEBUG
     setenv("EODebug", "1", 1);
     setenv("TLF_DEBUG", "1", 1);
+    setenv("TLF_AUTO_ENABLE", "1", 1);
     //setenv("EOCORE_MICLOUD_MACHINE", "1", 1);
 #endif
-    NSLog(@"Tealeaf SDK Loaded");
-    [[TLFApplicationHelper sharedInstance] enableTealeafFramework];
+    //NSLog(@"Tealeaf SDK Loaded");
+    //[[TLFApplicationHelper sharedInstance] enableTealeafFramework];
     
 }
 @end
