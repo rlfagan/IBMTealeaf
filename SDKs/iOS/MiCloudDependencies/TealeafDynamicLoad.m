b6085f3476ead8cc45823f65520400b59c82b63e
Shridhar Damale <sdamale@us.ibm.com>
2018-01-04 10:05:05 -0800

    Use @import Tealeaf

--- SDKs/iOS/MiCloudDependencies/TealeafDynamicLoad.m
+++ SDKs/iOS/MiCloudDependencies/TealeafDynamicLoad.m
@@ -1,5 +1,5 @@
 #import "TealeafDynamicLoad.h"
-#import "../Debug/Tealeaf.framework/Headers/TLFApplicationHelper.h"
+@import Tealeaf;
 @implementation TealeafDynamicLoad
 + (void)load {
 #ifdef DEBUG
