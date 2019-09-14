a66a6b651b3b4bf9e4e2af28285028c99781476f
Omar Hernandez <ohernandez@us.ibm.com>
2019-09-13 20:23:55 -0700

    Tealeaf beta build: 10.4.12
    Beta Change Notes:
    Enhancements
    
    - Added accessibility data to json payload - id, hint and label to type 4, type 10 and type 11
    - Added IPV6. IP address is already there in the payload. Configuration settings RemoveIp and IpPlaceholder will apply to both ip and ipv6
    - Added support for MaxNumberOfFilesToCache in case of archival of unsent json data. HasToPersistLocalCache has to be true to make it work. If HasToPersistLocalCache is false, nothing is cached to local storage like before.
    - Add support for multiple carriers in the json payload
    - Add exact name of the iOS device to json payload. It is called machineNameEnglish and machineNameHW and can be found in mobileEnvironment.
    - Added support for 'Dynamic Update' i.e. sending multiple layout type 10 messages for a single screen view load type 2 message. Before each type 10 would require its own type 2.
    - Code injection can now be turned off individually for : NSData, UIViewController, UITableView, UITableViewCell, UIImage, UIAlertAction, UIPageControl, UIPickerView, UIDatePicker, UIWebView, WKWebView. Look for NSObject+TealeafEarlyLoads.m file for details. Add those methods to your NSObject extensions.
    - Pixel Density for screenshots is now configurable via TealeafBasicConfig.plist ScreenShotPixelDensity setting. Default is 1.5. 0 or negative values are invalid.
    
    Bug Fixes
    
    - Fixed bugs related to layout config, enhanced replay, log levels.
    - Fixed an issue where mobile state was missing from json payload
    - Fixed issue related to wkactionsheet unload which was causing incorrect navlist and replay
    - Fixed issue related to PostMessageMaxBytesSize
    - Fixed React Native specific exception about textStorage
    - Fixed logEvent method to accept empty values dictionary
    
    Known Issues
    
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Alerts are broken on replay. It will be fixed in next version.

--- SDKs/iOS/CordovaDependencies/NSObject+TealeafEarlyLoads.m
+++ SDKs/iOS/CordovaDependencies/NSObject+TealeafEarlyLoads.m
@@ -1,9 +1,14 @@
 //
-//  Licensed Materials - Property of IBM
-//  (C) Copyright IBM Corp. 2016
-//  US Government Users Restricted Rights - Use, duplication or disclosure
-//  restricted by GSA ADP Schedule Contract with IBM Corp.
+// Copyright (C) 2016 Acoustic, L.P. All rights reserved.
 //
+// NOTICE: This file contains material that is confidential and proprietary to
+// Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+// industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+// Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+// prohibited.
+//
+
+/*Include this file into your xcode project's Target > Build Phases > Compile Sources as needed*/
 
 #import "NSObject+TealeafEarlyLoads.h"
 
@@ -12,4 +17,72 @@
 {
     return;
 }
+/*
+
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIViewController
++(void)tealeafDoNotInjectForUIViewController
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UITableView
++(void)tealeafDoNotInjectForUITableView
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UITableViewCell
++(void)tealeafDoNotInjectForUITableViewCell
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIImage
++(void)tealeafDoNotInjectForUIImage
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIAlertAction
++(void)tealeafDoNotInjectForUIAlertAction
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIPageControl
++(void)tealeafDoNotInjectForUIPageControl
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIPickerView
++(void)tealeafDoNotInjectForUIPickerView
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIDatePicker
++(void)tealeafDoNotInjectForUIDatePicker
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into NSData
++(void)tealeafDoNotInjectForNSData
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into UIWebView
++(void)tealeafDoNotInjectForUIWebView
+{
+    return;
+}
+ 
+ Uncomment this method if you do not want Tealeaf to automatically inject into WKWebView
++(void)tealeafDoNotInjectForWKWebView
+{
+    return;
+}
+*/
 @end
