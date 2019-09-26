9538bc8ebfacb62751c1155b0da21451897ae5db
Omar Hernandez <ohernandez@us.ibm.com>
2019-09-25 19:17:46 -0700

    Tealeaf beta build: 10.4.25
    Beta Change Notes:
    Enhancements
    
    - Added accessibility data to json payload - id, hint and label to type 4, type 10 and type 11
    - Added IPV6. IP address is already there in the payload. Configuration settings RemoveIp and IpPlaceholder will apply to both ip and ipv6
    - Added support for MaxNumberOfFilesToCache in case of archival of unsent json data. HasToPersistLocalCache has to be true to make it work. If HasToPersistLocalCache is false, nothing is cached to local storage like before.
    - Add support for multiple carriers in the json payload
    - Add exact name of the iOS device to json payload. It is called machineNameEnglish and machineNameHW and can be found in mobileEnvironment.
    - Added support for 'Dynamic Update' i.e. sending multiple layout type 10 messages for a single screen view load type 2 message. Before each type 10 would require its own type 2.
    - Code injection can now be turned off individually for : NSData, UIViewController, UITableView, UITableViewCell, UIImage, UIAlertAction, UIPageControl, UIPickerView, UIDatePicker, UIWebView, WKWebView. Look for NSObject+TealeafEarlyLoads.m file for details. Add those methods to your NSObject extensions.
    - Pixel Density for screenshots is now configurable via TealeafBasicConfig.plist ScreenShotPixelDensity setting. Default is 1.5. 0 or negative values are invalid.
    
    Bug Fixes
    
    - Fixed bugs related to layout config, enhanced replay, log levels.
    - Fixed an issue where mobile state was missing from json payload
    - Fixed issue related to wkactionsheet unload which was causing incorrect navlist and replay
    - Fixed issue related to PostMessageMaxBytesSize
    - Fixed React Native specific exception about textStorage
    - Fixed logEvent method to accept empty values dictionary
    
    Known Issues
    
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Alerts are broken on replay. It will be fixed in next version.

---
+++ SDKs/iOS/Debug/DAMod.framework/Headers/DAMod.h
@@ -0,0 +1,19 @@
+//
+//  DAMod.h
+//  DAMod
+//
+//  Created by Shridhar Damale on 30/10/17.
+//  Copyright © 2017 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DAMod.
+FOUNDATION_EXPORT double DAModVersionNumber;
+
+//! Project version string for DAMod.
+FOUNDATION_EXPORT const unsigned char DAModVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DAMod/PublicHeader.h>
+#import "DigitalAnalytics.h"
+
