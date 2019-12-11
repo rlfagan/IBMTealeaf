0b8817728dfc46f4b03b42481c628be497c8bed8
Omar Hernandez <ohernandez@us.ibm.com>
2019-12-10 17:10:09 -0800

    Tealeaf beta build: 10.5.3
    Beta Change Notes:
    Enhancements
    
    - Removed support for UIWebView due to reported Apple warnings when applications are submitted to AppStore:
      ITMS-90809: Deprecated API Usage.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.
    - arm64e bitcode slice is not created. Filed support ticket with Apple.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Alerts are broken on replay. It will be fixed in next version.

--- SDKs/iOS/CordovaDependencies/NSObject+TealeafEarlyLoads.h
+++ SDKs/iOS/CordovaDependencies/NSObject+TealeafEarlyLoads.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2016 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2019 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -24,6 +24,5 @@
 +(void)tealeafDoNotInjectForUIPickerView;
 +(void)tealeafDoNotInjectForUIDatePicker;
 +(void)tealeafDoNotInjectForNSData;
-+(void)tealeafDoNotInjectForUIWebView;
 +(void)tealeafDoNotInjectForWKWebView;
 @end
